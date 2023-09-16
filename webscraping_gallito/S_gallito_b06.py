1da1a407364d5b8aa38c83a6cc36f3414a3b61ca
Hentype85 <martinleiro9@gmail.com>
2023-09-15 18:18:04 -0700

    actualizacion de datos y su json generado  ( faltan imagenes )

--- webscraping_gallito/S_gallito_b06.py
+++ webscraping_gallito/S_gallito_b06.py
@@ -31,100 +31,110 @@ time.sleep(2)
 menu_alquileres = driver.find_element(By.XPATH, '//div[@id="cat_inmuebles"]/div[2]/ul/li[2]/h3/a')
 menu_alquileres.click()
 
-time.sleep(1)
+time.sleep(2)
 
 # lista en la que se van a guardar los datos de cada publicacion
 lst_data = []
 
 # capturar en rango desde la pagina 0 hasta la que determine range()
-for i in range(2):
+for i in range(5):
 
     if i > 0:
         # avanzo a la siguiente pagina
         avanzar_pag = driver.find_element(By.XPATH, '//div[@id="paginador"]/ul/li[6]/a')
         avanzar_pag.click()
+        time.sleep(4)
+
+    time.sleep(4)
+    # captura la lista de elementos de alquiler
+    lst_alquiler = driver.find_elements(By.XPATH, '//div[3]/div[1]/div/div[1]/a')
+
+    # creo una lista con las urls para que no pierda la informacion al hacer el back()
+    urls_alquiler = []
+    for alquiler in lst_alquiler:
+        url_alquiler = alquiler.get_attribute('href')
+        urls_alquiler.append(url_alquiler)
+
+    # abre cada uno de los url de alquileres
+    for url_alquiler in urls_alquiler:
+
+        # abrir enlace
+        driver.get(url_alquiler)
         time.sleep(2)
 
-    else:
-        time.sleep(4)
-        # captura la lista de elementos de alquiler
-        lst_alquiler = driver.find_elements(By.XPATH, '//div[3]/div[1]/div/div[1]/a')
-
-        # creo una lista con las urls para que no pierda la informacion al hacer el back()
-        urls_alquiler = []
-        for alquiler in lst_alquiler:
-            url_alquiler = alquiler.get_attribute('href')
-            urls_alquiler.append(url_alquiler)
-
-        # abre cada uno de los url de alquileres
-        for url_alquiler in urls_alquiler:
-
-            # abrir enlace
-            driver.get(url_alquiler)
+        # capturar informacion de url abierta
+        try:
+            id = "gallito_{}".format(url_alquiler.split("-")[-1])
+            precioString = driver.find_element(By.XPATH, '//div[@id="div_datosBasicos"]/div[2]/span').text
+            precio = float(precioString.split(" ")[-1].replace(".",""))
+            moneda = precioString.split(" ")[0]
+        except Exception:
+            pass
+        try:
+            departamento = driver.find_element(By.XPATH, '//*[@id="ol_breadcrumb"]/li[5]/a').text
+            zona = driver.find_element(By.XPATH, '//*[@id="ol_breadcrumb"]/li[6]/a').text
+        except Exception:
+            pass
+        try:
+            tipo_propiedad = driver.find_element(By.XPATH, '//div[@id="div_datosOperacion"]/div[1]/p').text
+        except:
+            tipo_propiedad = ""
+        try:
+            banos = driver.find_element(By.XPATH, '//div[@id="div_datosOperacion"]/div[5]/p').text
+        except Exception:
+            banos = ""
+            pass
+        try:
+            metros = driver.find_element(By.XPATH, '//div[@id="div_datosOperacion"]/div[6]/p').text
+        except Exception:
+            metros = ""
+            pass
+        try:
+            dormitorios = driver.find_element(By.XPATH, '//div[@id="div_datosOperacion"]/div[4]/p').text
+        except Exception:
+            dormitorios = ""
+            pass
+        try:
+            # voy a seccion de ubicacion
+            place_map = driver.find_element(By.XPATH, '//*[@id="ulNavGaleria"]/li[4]/a')
+            place_map.click()
             time.sleep(2)
 
-            # capturar informacion de url abierta
-            try:
-                id = "gallito_{}".format(url_alquiler.split("-")[-1])
-                precioString = driver.find_element(By.XPATH, '//div[@id="div_datosBasicos"]/div[2]/span').text
-                precio = float(precioString.split(" ")[-1].replace(".",""))
-                moneda = precioString.split(" ")[0]
-            except Exception:
-                pass
-            try:
-                departamento = driver.find_element(By.XPATH, '//*[@id="ol_breadcrumb"]/li[5]/a').text
-                zona = driver.find_element(By.XPATH, '//*[@id="ol_breadcrumb"]/li[6]/a').text
-                tipo_propiedad = driver.find_element(By.XPATH, '//div[@id="div_datosOperacion"]/div[1]/p').text
-            except Exception:
-                pass
-            try:
-                banos = driver.find_element(By.XPATH, '//div[@id="div_datosOperacion"]/div[5]/p').text
-                metros = driver.find_element(By.XPATH, '//div[@id="div_datosOperacion"]/div[6]/p').text
-                dormitorios = driver.find_element(By.XPATH, '//div[@id="div_datosOperacion"]/div[4]/p').text
-            except Exception:
-                pass
-            try:
-                # voy a seccion de ubicacion
-                place_map = driver.find_element(By.XPATH, '//*[@id="ulNavGaleria"]/li[4]/a')
-                place_map.click()
-                time.sleep(1)
+            url_map = driver.find_element(By.XPATH, '//*[@id="iframeMapa"]')
+            src = url_map.get_attribute('src')
+            lat = src.split("=")[2].split(",")[0][:11]
+            lon = src.split("=")[2].split(",")[-1][:11]
+        except Exception :
+            lat = 0
+            lon = 0
+            pass
 
-                url_map = driver.find_element(By.XPATH, '//*[@id="iframeMapa"]')
-                src = url_map.get_attribute('src')
-                lat = src.split("=")[2].split(",")[0][:11]
-                lon = src.split("=")[2].split(",")[-1][:11]
+        # guardar informacion en diccionario
+        try:
+            dic_alquiler = {
+                "id": id,
+                "url_link": url_alquiler,
+                "price": precio,
+                "exchange": str(moneda),
+                "state_name": departamento,
+                "city_name": zona,
+                "PROPERTY_TYPE": tipo_propiedad,
+                "TOTAL_AREA": metros,
+                "FULL_BATHROOMS": banos,
+                "BEDROOMS": dormitorios,
+                "location": {
+                    "latitude": float(lat),
+                    "longitude": float(lon)
+                },
+                "imagenes": []
+            }
+            lst_data.append(dic_alquiler)
+        except Exception:
+            pass
 
-
-            except Exception :
-                pass
-
-            # guardar informacion en diccionario
-            try:
-                dic_alquiler = {
-                    "id": id,
-                    "url_link": url_alquiler,
-                    "price": precio,
-                    "exchange": str(moneda),
-                    "state_name": departamento,
-                    "city_name": zona,
-                    "PROPERTY_TYPE": tipo_propiedad,
-                    "TOTAL_AREA": metros,
-                    "FULL_BATHROOMS": banos,
-                    "BEDROOMS": dormitorios,
-                    "location": {
-                        "latitude": float(lat),
-                        "longitude": float(lon)
-                    },
-                    "imagenes": []
-                }
-                lst_data.append(dic_alquiler)
-            except Exception:
-                pass
-
-            # volver a la pagina anterior
-            driver.back()
-            time.sleep(2)
-
+        # volver a la pagina anterior
+        driver.back()
+        time.sleep(2)
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
