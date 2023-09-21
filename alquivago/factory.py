b69e28eb9b2ffbff1023a409dfec1b35df60efbe
EmilianoGarin <60578409+EmilianoGarin@users.noreply.github.com>
2023-09-21 16:55:05 -0700

    backend v0.1

---
+++ alquivago/factory.py
@@ -0,0 +1,41 @@
+from flask.json import JSONEncoder
+import os
+
+from flask import Flask, render_template
+
+
+from flask_cors import CORS
+##from flask_bcrypt import Bcrypt
+##from flask_jwt_extended import JWTManager
+
+from bson import json_util, ObjectId
+from datetime import datetime, timedelta
+
+from alquivago.api.rent import rents_api_v1
+
+class MongoJsonEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return obj.strftime("%Y-%m-%d %H:%M:%S")
+        if isinstance(obj, ObjectId):
+            return str(obj)  # Convertir ObjectId a cadena de texto
+        return json_util.default(obj, json_util.CANONICAL_JSON_OPTIONS)
+
+def create_app():
+
+    APP_DIR = os.path.abspath(os.path.dirname(__file__))
+    STATIC_FOLDER = os.path.join(APP_DIR, 'build/static')
+    TEMPLATE_FOLDER = os.path.join(APP_DIR, 'build')
+
+    app = Flask(__name__, template_folder=TEMPLATE_FOLDER)
+    CORS(app)
+    app.json_encoder = MongoJsonEncoder
+    app.register_blueprint(rents_api_v1)
+    
+
+    @app.route('/', defaults={'path': ''})
+    @app.route('/<path:path>')
+    def serve(path):
+        return render_template('index.html')
+
+    return app
