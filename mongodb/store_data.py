3f67be10847a4efcd42ee7ff8b8b25298d785f79
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-09-22 00:29:58 -0300

    Store all collected data in a single collection

--- mongodb/store_data.py
+++ mongodb/store_data.py
@@ -5,27 +5,31 @@ import pymongo
 client = pymongo.MongoClient("mongodb://localhost:27017")
 db = client["alquivago"]
 
-# Create 3 collections
-coll_infocasas = db["coll_infocasas"]
-coll_gallito = db["coll_gallito"]
-coll_mercadolibre = db["coll_mercadolibre"]
+# Create a collection for collected data
+collected_data = db["collected_data"]
 
-# Read the 3 .json files
+# Read the 3 .json files and combine the data
+data = []
+
+# Read and append data from infocasas.json
 file1 = open("infocasas.json")
 data1 = json.load(file1)
 file1.close()
+data.extend(data1)
 
+# Read and append data from gallito.json
 file2 = open("gallito.json")
 data2 = json.load(file2)
 file2.close()
+data.extend(data2)
 
+# Read and append data from mercadolibre.json
 file3 = open("mercadolibre.json")
 data3 = json.load(file3)
 file3.close()
+data.extend(data3)
 
-# Insert the data into the MongoDB collections
-coll_infocasas.insert_many(data1)
-coll_gallito.insert_many(data2)
-coll_mercadolibre.insert_many(data3)
+# Insert the combined data into the MongoDB collection
+collected_data.insert_many(data)
 
-print("Data successfully stored in MongoDB!")
+print("Data from all three files successfully stored in the 'collected_data' collection in MongoDB!")
