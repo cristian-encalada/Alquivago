39dfbc03400a40461047c260cc7363c071d2ff2b
Gabr1el20 <6327@holbertonstudents.com>
2023-10-07 23:48:37 +0000

    Creando componente publicacion

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,57 +1,46 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import "./index.css"
+import gallitoLogo from './assets/gallito-logo.png'
+import infocasasLogo from './assets/infocasas-logo.jpg'
+import mercadoLibreLogo from './assets/mercadolibre-logo.png'
 
-function App({propertyType, propertyBathrooms, propertyBedrooms, propertyZone, propertyArea, propertyPrice, propertyCurrency, propertyImage}) {
+function App({propertyType, propertyBathrooms, propertyBedrooms, propertyZone, propertyArea, propertyPrice, propertyCurrency, propertyLink, propertyImage, propertyOrigin}) {
+  const originBackground = {
+    infocasas: infocasasLogo,
+    gallito: gallitoLogo,
+    mercadolibre: mercadoLibreLogo
+  }
+  const Background = originBackground[propertyOrigin];
   return (
-    <section className='flex'>
-    <div className="max-w-sm rounded overflow-hidden shadow-lg hover:shadow-xl g flex flex-wrap">
-      <img className="w-full h-1/2" src={propertyImage} alt="Property Image" />
-      <div className="px-6 py-4">
-        <div className="mb-2">
-          <h2 className="text-xl font-bold text-gray-900 text-center">{propertyType} en {propertyZone}</h2>
-          <div className="flex items-center">
-            <div className="mr-2 rounded-full bg-blue-600 py-1 px-2 text-xs font-medium text-white">{propertyType}</div>
-            <div className="rounded-full bg-yellow-500 py-1 px-2 text-xs font-medium text-white">{propertyZone}</div>
-          </div>
+    <div className="m-auto border-2 border-slate-500 shadow-lg flex w-[80%] max-w-[950px] h-52 justify-start rounded-2xl">
+        <div className="w-1/3 max-h-full max-w-full rounded-tl-2xl rounded-bl-2xl bg-no-repeat bg-center bg-cover flex flex-col justify-end items-end" style={{
+        background: `url(${propertyImage})`,
+        backgroundSize: 'cover', // Añade esta línea para ajustar la imagen al contenedor
+      }}>
+        <div className='h-14 w-14 relative mb-1 mr-1 rounded-xl' style={{
+          background: `url(${Background})`,
+          backgroundSize: 'contain',
+        }}></div>
         </div>
-        <div className="flex justify-between">
-          <div className="flex items-center">
-            <img src="https://img.icons8.com/windows/24/null/bedroom.png" alt="Bedroom Icon" />
-            <p className="ml-2 text-sm font-medium text-gray-700">{propertyBathrooms} Bedrooms</p>
-          </div>
-          <div className="flex items-center">
-            <img src="https://img.icons8.com/pastel-glyph/24/null/bath--v2.png" alt="Bathroom Icon" />
-            <p className="ml-2 text-sm font-medium text-gray-700">{propertyBedrooms} Bathrooms</p>
-          </div>
-          <div className="flex items-center">
-            <img src="https://img.icons8.com/ios-glyphs/24/null/expand--v1.png" alt="Area Icon" />
-            <p className="ml-2 text-sm font-medium text-gray-700">{propertyArea} sqm</p>
-          </div>
+        <div className="p-3 flex flex-col justify-around" style={{ flex: 1}}>
+            <h3 className="text-center m-0">{propertyType} en {propertyZone}</h3>
+            <p>{propertyType}</p>
+            <p>{propertyZone}</p>
+            <span className="text-center font-extrabold font-serif">{propertyCurrency} {propertyPrice}</span>
         </div>
-        <div className="mt-4">
-          <p className="text-3xl font-extrabold text-blue-800 text-center">{propertyCurrency} {propertyPrice}</p>
+        <div className="w-64 flex flex-col">
+            <div className="bg-[#607399] bg-opacity-60
+             flex flex-col gap-4 justify-center rounded-2xl mt-1 border-2 h-[150px]">
+                <p className="left-3 relative text-white font-semibold">{propertyBathrooms} Baño{propertyBathrooms > 1? 's' : ''}</p>
+                <p className="left-3 relative text-white font-semibold">{propertyBedrooms} Dormitorio{propertyBedrooms >1? 's' : ''}</p>
+                <p className="left-3 relative text-white font-semibold">{propertyArea !== 0? `${propertyArea} mt2` : 'No hay datos sobre el área'}</p>
+            </div>
+            <div className="bg-[#CCBC82] text-white font-semibold border-4 flex items-center justify-center m-auto w-36 h-14 rounded-2xl">
+                <button className="text-left"><a href={propertyLink}>Consultar</a></button>
+            </div>
         </div>
-      </div>
-      <div className="px-6 py-4 flex justify-between items-center">
-        <div className="flex items-center">
-          <img src="https://via.placeholder.com/50" alt="Agent Image" className="mr-2 rounded-full object-cover" />
-          <div>
-            <p className="text-sm font-medium text-gray-800">Agent Name</p>
-            <p className="text-xs text-gray-600">Real Estate Agent</p>
-          </div>
-        </div>
-        <div className="flex">
-          <a href="tel:+1234567890" className="mr-2 rounded-full bg-gray-300 p-1 text-gray-700 hover:text-gray-800">
-            <img src="https://img.icons8.com/color/24/null/ringer-volume.png" alt="Phone Icon" />
-          </a>
-          <a href="https://wa.me/1234567890" className="rounded-full bg-green-500 p-1 text-white hover:bg-green-600">
-            <img src="https://img.icons8.com/external-those-icons-lineal-color-those-icons/24/null/external-WhatsApp-social-media-those-icons-lineal-color-those-icons.png" alt="WhatsApp Icon" />
-          </a>
-        </div>
-      </div>
     </div>
-    </section>
   );
 };
 export default App
