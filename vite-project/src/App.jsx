56466346cfd23a43547d95989737b1442e95d85b
Gabr1el20 <6327@holbertonstudents.com>
2023-10-09 20:20:45 +0000

    Added publish component-made it responsive-

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -13,34 +13,40 @@ function App({propertyType, propertyBathrooms, propertyBedrooms, propertyZone, p
   }
   const Background = originBackground[propertyOrigin];
   return (
-    <div className="m-auto border-2 border-slate-500 shadow-lg flex w-[80%] max-w-[950px] h-52 justify-start rounded-2xl">
-        <div className="w-1/3 max-h-full max-w-full rounded-tl-2xl rounded-bl-2xl bg-no-repeat bg-center bg-cover flex flex-col justify-end items-end" style={{
-        background: `url(${propertyImage})`,
-        backgroundSize: 'cover', // Añade esta línea para ajustar la imagen al contenedor
-      }}>
-        <div className='h-14 w-14 relative mb-1 mr-1 rounded-xl' style={{
-          background: `url(${Background})`,
-          backgroundSize: 'contain',
-        }}></div>
-        </div>
-        <div className="p-3 flex flex-col justify-around" style={{ flex: 1}}>
-            <h3 className="text-center m-0">{propertyType} en {propertyZone}</h3>
-            <p>{propertyType}</p>
-            <p>{propertyZone}</p>
-            <span className="text-center font-extrabold font-serif">{propertyCurrency} {propertyPrice}</span>
-        </div>
-        <div className="w-64 flex flex-col">
-            <div className="bg-[#607399] bg-opacity-60
-             flex flex-col gap-4 justify-center rounded-2xl mt-1 border-2 h-[150px]">
-                <p className="left-3 relative text-white font-semibold">{propertyBathrooms} Baño{propertyBathrooms > 1? 's' : ''}</p>
-                <p className="left-3 relative text-white font-semibold">{propertyBedrooms} Dormitorio{propertyBedrooms >1? 's' : ''}</p>
-                <p className="left-3 relative text-white font-semibold">{propertyArea !== 0? `${propertyArea} mt2` : 'No hay datos sobre el área'}</p>
-            </div>
-            <div className="bg-[#CCBC82] text-white font-semibold border-4 flex items-center justify-center m-auto w-36 h-14 rounded-2xl">
-                <button className="text-left"><a href={propertyLink}>Consultar</a></button>
-            </div>
-        </div>
+    <div className="m-auto mt-10 flex h-80 w-5/6 flex-col rounded-2xl bg-slate-200 shadow-2xl lg:flex-row">
+  <div className="flex h-3/4 max-w-full flex-col items-end justify-end rounded-tl-2xl rounded-tr-2xl bg-center lg:h-full lg:w-1/3 lg:rounded-none lg:rounded-bl-2xl lg:rounded-tl-2xl" style={{
+    background: `url(${propertyImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  }}>
+    <div className="relative mb-3 mr-5 h-8 w-8 rounded-lg lg:h-12 lg:w-12" style={{
+      background: `url(${Background})`,
+      backgroundSize: 'cover',
+    }}></div>
+  </div>
+  <h1 className="w-full py-1 text-center text-xl font-medium lg:hidden">{propertyType} en {propertyZone}</h1>
+  <div className="flex h-full lg:w-full lg:flex-row-reverse gap-2">
+    <div className="bg-red flex h-full w-1/2 flex-col lg:w-1/4">
+      <div className="my-2 flex h-2/3 flex-col justify-around rounded-2xl bg-[#414C67] py-3 pl-5 lg:pl-12">
+        <p className="font-medium text-white">{propertyBedrooms > 1? `${propertyBedrooms} Dorm.`: 'Monoambiente'}</p>
+        <p className="font-medium text-white">{propertyBathrooms > 1? `${propertyBathrooms} Baños`: '1 Baño'}</p>
+        <p className="font-medium text-white">{propertyArea > 0? `${propertyArea} mt2`: 'No info'}</p>
+      </div>
+      <button className="mx-1 mb-1 flex flex-row-reverse items-center justify-center rounded-3xl bg-[#CCBC82] px-4 py-2 font-bold text-white hover:bg-[#AF9A4B] lg:h-1/3">
+        <svg className="h-10 w-7" fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5"></path>
+        </svg>
+        <a target='_blank' href={propertyLink}>Consultar</a>
+      </button>
     </div>
+    <div className="mt-3 flex h-full w-1/2 flex-col items-center gap-5 lg:w-3/4 lg:justify-center lg:gap-10">
+      <h1 className="hidden text-xl font-medium lg:inline">{propertyType} en {propertyZone}</h1>
+      <span className="w-full rounded-3xl bg-white py-2 text-center font-medium lg:w-2/3">{propertyType}</span>
+      <span className="w-full rounded-3xl bg-white py-2 text-center font-medium lg:w-2/3">{propertyZone}</span>
+      <p className="lg:b mb-2 text-2xl font-semibold lg:relative">{propertyCurrency} {propertyPrice}</p>
+    </div>
+  </div>
+</div>
   );
 };
 export default App
