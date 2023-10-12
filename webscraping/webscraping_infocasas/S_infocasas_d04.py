68967141192b8f22d0c1d84c1c8ba44e06411900
martin <martinleiro9@gmail.com>
2023-10-12 17:44:58 +0000

    fix2

--- webscraping/webscraping_infocasas/S_infocasas_d04.py
+++ webscraping/webscraping_infocasas/S_infocasas_d04.py
@@ -240,10 +240,15 @@ for i in range(2):
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 
+# directorio de guardado
+jsonPath = 'root/Alquivago/webscraping/webscraping_infocasas/infocasas.json'
+
 # exportar JSON
 json_data = json.dumps(lst_data, indent=4, ensure_ascii=False)
-with open('infocasas.json', 'w', encoding='utf-8') as json_file:
+with open(jsonPath, 'w', encoding='utf-8') as json_file:
     json_file.write(json_data)
+
+# input("Enter para salir..")
 
 # cerrar driver
 driver.quit()
