de977b56ca19049edb27a6d8f36940a3d3561e65
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-10-13 09:50:40 -0300

    Update run.py MONGO_URI to point the latest Atlas database

--- api/run.py
+++ api/run.py
@@ -9,6 +9,7 @@ config = configparser.ConfigParser()
 if __name__ == "__main__":
     app = create_app()
     app.config['DEBUG'] = True
-    app.config['MONGO_URI'] = "mongodb+srv://alice:CheshireCat@prueba.emolc4y.mongodb.net/alquivago?retryWrites=true&w=majority"
+    # app.config['MONGO_URI'] = "mongodb+srv://alice:CheshireCat@prueba.emolc4y.mongodb.net/alquivago?retryWrites=true&w=majority"
+    app.config['MONGO_URI'] = "mongodb+srv://alquivago:alquivago123@cluster0.hhicxbc.mongodb.net/?retryWrites=true&w=majority"
 
     app.run(host='0.0.0.0', port=5000)
