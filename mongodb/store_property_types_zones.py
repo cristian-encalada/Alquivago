035030cc4b4338995f371d9578d05e619f5785e3
Cristian Encalada <yourncris.encalada.camargo@gmail.com>
2023-10-18 08:50:14 -0700

    Update store_property_types_zones.py, done

--- mongodb/store_property_types_zones.py
+++ mongodb/store_property_types_zones.py
@@ -8,8 +8,8 @@ client = pymongo.MongoClient(atlas_uri)
 db = client["alquivago"]
 
 # URLs of the JSON files on GitHub
-property_types_url = "https://raw.githubusercontent.com/cristian-encalada/Alquivago/dev/mongodb/property_types.json"
-zonas_montevideo_url = "https://raw.githubusercontent.com/cristian-encalada/Alquivago/dev/mongodb/zonas_montevideo.json"
+property_types_url = "https://raw.githubusercontent.com/cristian-encalada/Alquivago/dev/data/property_types.json"
+zonas_montevideo_url = "https://raw.githubusercontent.com/cristian-encalada/Alquivago/dev/data/zonas_montevideo.json"
 
 # Define the names of the MongoDB collections
 property_types_col = db["property_types_col"]
@@ -20,7 +20,18 @@ def insert_json_into_mongo(url, collection):
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
-        collection.insert_one(data)
+        # Check if the data contains a key indicating a list of dictionaries
+        if "property_types" in data:
+            data_list = data["property_types"]
+        elif "zonas" in data:
+            data_list = data["zonas"]
+        else:
+            print(f"Data from {url} does not match the expected structure")
+            return
+
+        # Insert each dictionary as a separate document
+        for item in data_list:
+            collection.insert_one(item)
         print(f"Data from {url} inserted into MongoDB collection: {collection.name}")
     else:
         print(f"Failed to retrieve data from {url}")
