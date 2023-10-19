88244140844429b1eb682322b7f5dd9720790df4
EmilianoGarin <60578409+EmilianoGarin@users.noreply.github.com>
2023-10-19 14:37:01 -0700

    backend v0.9.1 (update Dokerfile and app.py CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,6 @@
 """ Flask Application """
 from flask import Flask, Blueprint, jsonify, make_response
+from flask_cors import CORS
 from api.v1.views import app_views
 from flasgger import Swagger
 from flasgger.utils import swag_from
@@ -11,7 +12,7 @@ app.register_blueprint(app_views)
 # _v1 = Blueprint('api_v1', 'api_v1', url_prefix='/api/v1')
 # app.register_blueprint(api_v1)
 app.config['MONGO_URI'] = "mongodb+srv://alquivago:alquivago123@cluster0.hhicxbc.mongodb.net/alquivago?retryWrites=true&w=majority"
-
+CORS(app)
 
 @app.route('/')
 def home():
