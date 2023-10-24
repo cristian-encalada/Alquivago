da246cb452bd70ad5fb3c881449bb2c3d7a49aec
Gabriel Delgado <6327@holbertonstudents.com>
2023-10-24 20:31:58 +0000

    njf

--- alquivago-nextjs/app/publish/[moneda]/page.jsx
+++ alquivago-nextjs/app/publish/[moneda]/page.jsx
@@ -1,9 +1,9 @@
 import InfiniteScroll from "../infinite-scroll-posts";
 import getData from "../actions";
+import { usePathname, useSearchParams } from "next/navigation";
 
 export default async function MonedaPage({ params }) {
   const posts = await getData(1, decodeURIComponent(params.moneda))
-  console.log(decodeURIComponent(params.moneda))
   return (
     <>
     <InfiniteScroll firstPage={posts} currencyFilter={decodeURIComponent(params.moneda)}/>
