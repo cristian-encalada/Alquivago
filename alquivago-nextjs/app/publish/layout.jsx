a0485a1d142434bbca652abc8244c1db7e89d93f
Gabriel Delgado <6327@holbertonstudents.com>
2023-10-25 20:20:37 +0000

    Added type filters

--- alquivago-nextjs/app/publish/layout.jsx
+++ alquivago-nextjs/app/publish/layout.jsx
@@ -1,12 +1,14 @@
 'use client'
 import { useRouter } from "next/navigation"
 import CurrencyButton from "../components/CurrencyButton"
+import TypeButton from "../components/TypeButton"
 export default function PublishLayout({ children }) {
   const router = useRouter()
   return (
     <>
-    <section className="flex justify-center">
+    <section className="flex justify-center items-start gap-5">
       <CurrencyButton/>
+      <TypeButton />
     </section>
     {children}
     </>
