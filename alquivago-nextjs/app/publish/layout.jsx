9e696502ecab00e6ce1b4e489212c93c69382a0b
Gabriel Delgado <6327@holbertonstudents.com>
2023-10-25 20:33:33 +0000

    Added bathrooms filters

--- alquivago-nextjs/app/publish/layout.jsx
+++ alquivago-nextjs/app/publish/layout.jsx
@@ -2,13 +2,15 @@
 import { useRouter } from "next/navigation"
 import CurrencyButton from "../components/CurrencyButton"
 import TypeButton from "../components/TypeButton"
+import RangeInput from "../components/RangeInput"
 export default function PublishLayout({ children }) {
   const router = useRouter()
   return (
     <>
     <section className="flex justify-center items-start gap-5">
-      <CurrencyButton/>
+      <CurrencyButton />
       <TypeButton />
+      <RangeInput />
     </section>
     {children}
     </>
