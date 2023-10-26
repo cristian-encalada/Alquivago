21962f4019cbddf17677120152b9149bf71daeea
Martin Leiro <martinleiro9@gmail.com>
2023-10-26 16:29:58 +0000

    fix gallito

--- webscraping/webscraping_gallito/S_gallito_v00.py
+++ webscraping/webscraping_gallito/S_gallito_v00.py
@@ -1,10 +1,13 @@
 # scraping gallito
+
 import platform
 import time
 import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+
+
 # Obtener el nombre del sistema operativo
 sistema_operativo = platform.system()
 
@@ -64,6 +67,12 @@ lst_data = []
 # capturar en rango desde la pagina 0 hasta la que determine range()
 for i in range(2):
 
+    if i > 0:
+        # se crea url cambiando el ultimo char de la url
+        custom_url_avanzando = f"{custom_avanzar[:-1]}{i + 1}"
+        driver.get(custom_url_avanzando)
+        time.sleep(2)
+    
     # captura la lista de elementos de alquiler
     lst_alquiler = driver.find_elements(By.XPATH, '//div[3]/div[1]/div/div[1]/a')
 
@@ -76,9 +85,10 @@ for i in range(2):
     # abre cada uno de los url de alquileres
     for j, url_alquiler in enumerate(urls_alquiler):
 
-        # abrir enlace
         driver.get(url_alquiler)
         time.sleep(2)
+
+        print(f"pagina: {i} alquiler: {j}")
 
         # capturar informacion de url abierta
         try:
@@ -172,15 +182,14 @@ for i in range(2):
         except Exception:
             pass
 
-        # back() volver
+        # abrir enlace
         if i == 0:
-            # pagina 0 de alquileres
+            # volver pagina 0 de alquileres
             driver.get(selecDepart)
         else:
-            # se crea url cambiando el ultimo char de la url
-            custom_url_avanzando = f"{custom_avanzar[:-1]}{i + 1}"
+            # volver pagina distinta a 0
             driver.get(custom_url_avanzando)
-       
+
         time.sleep(2)
 
 #---------------------------------------------------------------
