cc1fa5717442445015701bf911e7e46e0ea40cdf
Gabriel Delgado <6327@holbertonstudents.com>
2023-10-30 20:58:55 +0000

    Stylished a little bit mobile filters

--- alquivago-nextjs/app/components/MobileFilter.jsx
+++ alquivago-nextjs/app/components/MobileFilter.jsx
@@ -72,9 +72,10 @@ export default function MobileFilter() {
         setFilters([...filters, value]);
       }
     }
-  
-  
-
+  const removeFilters = () => {
+    setFilters([])
+    return router.push('/publish')
+  }
   const handleApplyFilters = () => {
     // Aquí puedes usar el estado actualizado filters para realizar acciones con los filtros seleccionados
     const routeParsed = formatearArrayComoString(filters)
@@ -84,7 +85,7 @@ export default function MobileFilter() {
 
   return (
     <>
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleVisibility}>Filtros</button>
+    <button class="bg-dorado-200 hover:bg-dorado-500 w-4/6 text-white font-bold py-2 px-6 rounded" onClick={handleVisibility}>Filtros</button>
     <section className={`md:hidden ${visible} fixed z-30 inset-0 m-auto h-screen w-full flex-col justify-center items-center bg-slate-200`}>
       <div className="absolute top-0 right-0" onClick={handleVisibility}>Cerrar</div>
     <div className={`flex-col items-center justify-center gap-2 text-center text-xl font-medium text-slate-600 w-full`}>
@@ -96,7 +97,7 @@ export default function MobileFilter() {
     </ul>
     <div className="h-0.5 w-full bg-slate-500"></div>
     <h1>Tipos de vivienda</h1>
-    <div className="w-full flex justify-center my-3" >
+    <div className="w-full flex justify-center my-3 gap-2" >
     <button type="button" className="flex w-1/3 flex-grow rounded-full ring-blue-500 focus:outline-none focus:ring-2" data-value="tipos=0" onClick={handleTiposViviendaChange}>
       <span className="text-m inline-flex h-9 w-full items-center justify-center rounded-full border border-blue-300 bg-blue-300 font-bold leading-none text-blue-900 hover:border-blue-900 group-hover:border-blue-900">Apartamento</span>
     </button>
@@ -109,15 +110,15 @@ export default function MobileFilter() {
     </div>
     <div className="flex h-0.5 w-full bg-slate-500"></div>
     <h1>Dormitorios</h1>
-    <ul className="flex gap-4">
-      <li><input type="checkbox" value="dormitorios=1" className="mr-2" onChange={handleDormitoriosChange} checked={filters.includes('dormitorios=1')} />1 Dormitorio</li>
-      <li><input type="checkbox" value="dormitorios=2" className="mr-2" onChange={handleDormitoriosChange} checked={filters.includes('dormitorios=2')} />2 Dormitorios</li>
-      <li><input type="checkbox" value="dormitorios=3" className="mr-2" onChange={handleDormitoriosChange} checked={filters.includes('dormitorios=3')}/>3 Dormitorios</li>
-      <li><input type="checkbox" value="dormitorios=4" className="mr-2" onChange={handleDormitoriosChange} checked={filters.includes('dormitorios=4')}/>3+ Dormitorios</li>
+    <ul className="flex container gap-2">
+      <li><input type="checkbox" value="dormitorios=1" className="mr-2" onChange={handleDormitoriosChange} checked={filters.includes('dormitorios=1')} />1 Dorm.</li>
+      <li><input type="checkbox" value="dormitorios=2" className="mr-2" onChange={handleDormitoriosChange} checked={filters.includes('dormitorios=2')} />2 Dorm.</li>
+      <li><input type="checkbox" value="dormitorios=3" className="mr-2" onChange={handleDormitoriosChange} checked={filters.includes('dormitorios=3')}/>3 Dorm.</li>
+      <li><input type="checkbox" value="dormitorios=4" className="mr-2" onChange={handleDormitoriosChange} checked={filters.includes('dormitorios=4')}/>3+ Dorm.</li>
     </ul>
     <div className="flex h-0.5 w-full bg-slate-500"></div>
     <h1>Baños</h1>
-    <ul className="flex gap-4">
+    <ul className="flex gap-4 justify-center">
       <li><input type="checkbox" value="baños=1" className="mr-2" onChange={handleBaniosChange} checked={filters.includes('baños=1')}/>1 Baño</li>
       <li><input type="checkbox" value="baños=2" className="mr-2" onChange={handleBaniosChange} checked={filters.includes('baños=2')}/>2 Baños</li>
       <li><input type="checkbox" value="baños=3" className="mr-2" onChange={handleBaniosChange} checked={filters.includes('baños=3')}/>2+ Baños</li>
@@ -125,7 +126,7 @@ export default function MobileFilter() {
     <div className="h-0.5 w-full bg-slate-500"></div>
   <button type="button" className="mt-5 py-3 w-1/3 flex-grow rounded-full ring-blue-500 focus:outline-none focus:ring-2 bg-blue-300 text-center hover:scale-125 transition" onClick={handleApplyFilters}>Aplicar filtros</button>
   </div>
-  <button onClick={() => setFilters([])}>Limpiar filtros</button>
+  <button className="mt-5 py-3 w-1/3 aspect-ratio rounded-full ring-red-500 focus:outline-none focus:ring-2 bg-red-300 text-center hover:scale-125 transition text-red-700" onClick={removeFilters}>Limpiar filtros</button>
 </section>
 </>
   )
