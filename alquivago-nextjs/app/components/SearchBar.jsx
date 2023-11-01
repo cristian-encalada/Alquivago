298300d113103a7d622e300c7c6eeece67f0db32
Gabriel Delgado <6327@holbertonstudents.com>
2023-11-01 19:32:40 +0000

    Added cookies support, limit is 4 properties

--- alquivago-nextjs/app/components/SearchBar.jsx
+++ alquivago-nextjs/app/components/SearchBar.jsx
@@ -15,15 +15,15 @@ export default function SearchBar() {
   };
 
   return (
-    <section className='container flex-col lg:flex-row lg:w-1/2 justify-center items-center gap-3 lg:h-20 lg:gap-2 h-48 bg-transparent lg:mb-36 rounded-2xl shadow-xl flex'>
+    <section className='container flex-col lg:flex-row lg:w-2/3 justify-center items-center lg:h-20 h-48 bg-transparent lg:mb-36 rounded-2xl shadow-xl flex'>
       <input
-        className='lg:w-5/6 w-full h-full lg:rounded-l-2xl rounded-2xl text-center font-medium text-2xl'
+        className='lg:w-5/6 w-full h-full lg:rounded-none lg:rounded-l-2xl rounded-2xl text-center font-medium text-2xl'
         type="search"
         onChange={handleChange} // Utiliza onChange para manejar cambios en el input
       />
-      <div className="lg:w-1/6 w-full h-full lg:rounded-r-2xl rounded-2xl bg-azul-600">
+      <div className="lg:w-1/6 w-full h-full lg:rounded-none lg:rounded-r-2xl rounded-2xl bg-azul-600 hover:scale-110 transition">
         <button
-          className="text-center w-full h-full hover:scale-125 transition font-medium text-white text-lg"
+          className="text-center w-full h-full font-medium text-white text-lg"
           onClick={handleClick}
         >
           Buscar zona
