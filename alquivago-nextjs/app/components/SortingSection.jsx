4b12b559b5d18e15b062058ecc742a24e784e9d5
Gabr1el20 <6327@holbertonstudents.com>
2023-11-16 19:36:54 -0300

    Some minor improvements again

--- alquivago-nextjs/app/components/SortingSection.jsx
+++ alquivago-nextjs/app/components/SortingSection.jsx
@@ -1,7 +1,6 @@
 'use client'
 import { useState } from "react"
 import mapIcon from '../../public/mapIcon.svg'
-import listIcon from '../../public/listIcon.svg'
 import Link from "next/link"
 import Image from "next/image"
 import PriceOrder from "./PriceSort"
@@ -17,7 +16,7 @@ export default function OrderingSection() {
   function handleOrder(e, type) {
     const value = e.target.value;
     if (!orders.includes(value)) {
-      let updatedOrders = orders.filter(order => !order.startsWith(type)); // Eliminar cualquier opción de precio existente
+      let updatedOrders = orders.filter(order => !order.startsWith(type)); // Eliminar cualquier opción de precio/area existente
       setOrders([...updatedOrders, value]);
    } else {
       setOrders(orders.filter((order) => order != value))
@@ -34,6 +33,7 @@ export default function OrderingSection() {
   });
   }
   return (
+    <>
     <section className="w-full h-24 gap-5 bg-white md:first-letter flex justify-around md:justify-center relative">
       <PriceOrder orders={orders} setOrders={setOrders} handleOrder={handleOrder}>Ordenar Precio</PriceOrder>
       <AreaOrder orders={orders} setOrders={setOrders} handleOrder={handleOrder}>Ordenar Área</AreaOrder>
@@ -41,5 +41,15 @@ export default function OrderingSection() {
           <li><Link href='/map'><Image src={mapIcon} className="w-8 h-8 hover:scale-125 transition" alt="map icon"/></Link> </li>
         </ul>
     </section>
+    <div className='md:hidden w-full sticky h-10 top-0 bg-white  dark:bg-white z-40 rounded aspect-square flex justify-around items-center'>
+      <span className="w-1/2 flex justify-center border-2 h-full rounded-lg text-lg font-medium text-azul-600 animate-pulse items-center" onClick={() => {
+        if (!localStorage.getItem('arrData')) {
+          alert('No has seleccionado publicaciones para comparar')
+        } else router.push('/comparisons')
+      }}>
+        Comparar</span>
+      <Link href='/map' className="w-1/2 flex justify-center border-2 h-full rounded-lg"><Image src={mapIcon} alt='map icon' width={40} height={40} className="sticky top-0"/></Link>
+      </div>
+    </>
   )
 }
